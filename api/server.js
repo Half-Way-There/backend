7d5b421694f258a1539755ea92703e32654cef2d
James <info@thecodediver.com>
2020-11-23 16:41:14 -0500

    testing fix

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors");
-const admin = require('./firebase-admin/admin');
+// const admin = require('./firebase-admin/admin');
 const Auth = require("./routes/auth/auth-router.js")
 const Users = require("./routes/users/users-router.js")
 const Locations = require("./routes/locations/locations-router.js")
@@ -10,22 +10,22 @@ const server = express();
 server.use(express.json())
 server.use(cors());
 
-async function verifyToken(req, res, next) {
-  const idToken = req.headers.authorization
-  try {
-    const decodedToken = await admin.auth().verifyIdToken(idToken)
+// async function verifyToken(req, res, next) {
+//   const idToken = req.headers.authorization
+//   try {
+//     const decodedToken = await admin.auth().verifyIdToken(idToken)
 
-    if(decodedToken) {
-      req.body.uid = decodedToken.uid
-      return next()
-    } else {
-      return res.status(401).send("You are not authorized")
-    }
-  } catch (error) {
-    console.log(error)
-    return res.status(401).send("What happened!")
-  }
-}
+//     if(decodedToken) {
+//       req.body.uid = decodedToken.uid
+//       return next()
+//     } else {
+//       return res.status(401).send("You are not authorized")
+//     }
+//   } catch (error) {
+//     console.log(error)
+//     return res.status(401).send("What happened!")
+//   }
+// }
 
 // server.use("/", verifyToken)
 server.use("/auth", Auth)
