02046fe046d66d0d37e4b9af54aaa53555a696d8
James <info@thecodediver.com>
2020-11-24 09:34:30 -0500

    changed truncate to del in seed

--- database/seeds/02-addresses.js
+++ database/seeds/02-addresses.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('addresses').truncate()
+  return knex('addresses').del()
     .then(function () {
       // Inserts seed entries
       return knex('addresses').insert([
