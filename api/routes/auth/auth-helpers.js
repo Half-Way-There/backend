153c0fd087383b0be3bb3d92640f9056d832c1af
James <info@thecodediver.com>
2020-11-28 20:31:29 -0500

    fix login bug

--- api/routes/auth/auth-helpers.js
+++ api/routes/auth/auth-helpers.js
@@ -10,13 +10,14 @@ function addUser(uid) {
 }
 
 async function getUser(uid) {
-  const user = await db("users").select("uid", "address").where("uid", uid).first()
+  const user = await db("users").select("uid", "address").where("uid", uid)
   const categories = await db("categories").select("id", "category", "userId").where("userId", uid)
   const addresses = await db("addresses").select("addressId", "name as contactName", "address").where("userId", uid)
   const locations = await db("locations").select("locationId", "addressBookId", "name", "address", "website", "phone", "picture").where( "userId", uid)
+  
   return {
     user: {
-      ...user,
+      ...user[0],
       categories: categories
     },
     contacts: addresses.map(contact => {
